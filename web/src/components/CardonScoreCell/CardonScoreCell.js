967e349574c2b33b16599c89b3a4c63c5718f1b8
hassanuahmad <ahmad12p@uwindsor.ca>
2022-11-24 23:08:15 -0500

    fixed toml for redwood

--- web/src/components/CardonScoreCell/CardonScoreCell.js
+++ web/src/components/CardonScoreCell/CardonScoreCell.js
@@ -16,10 +16,7 @@ export const Loading = () => <div>Loading...</div>
 export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => (
-  <div style={{ color: 'red' }}>
-    Error: {error?.message}
-    {console.log(error)}
-  </div>
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
 
 export const Success = ({ cardonScore }) => {
